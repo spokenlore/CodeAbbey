fba173985d694cea00a413b70b51f97133dcf424
Samson <spokenlore@gmail.com>
2014-09-30 19:20:53 -0400

    CodeAbbey Unsynced Changes

---
+++ Beginner/23. fibSequence/fibSequence.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int fibSequence();
+
+int main(){
+	fstream infile;
+	infile.open("fibSequence.txt");
+	infile << int numFibs;
+
+	outfile.open("result.txt",fstream::out)
+}
